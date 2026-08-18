3a4e033352d166879862331d37cc0a4711072e3b
unoegeel <leegeonu02@gmail.com>
2026-08-18 11:16:45 +0900

    feat: 사용 가이드 이미지 갱신 및 조리 중 안내 문구 수정

--- FE/src/pages/user/OrderStatusPage.tsx
+++ FE/src/pages/user/OrderStatusPage.tsx
@@ -122,7 +122,7 @@ export const OrderStatusPage: React.FC = () => {
     ? "주문이 취소되었습니다."
     : isReadyLike
       ? "음식을 픽업해주세요."
-      : "음식이 준비되고 있습니다.";
+      : "맛있게 조리 중입니다. 잠시만 기다려 주세요!";
   const waitingAheadLabel = isReadyLike
     ? "없음"
     : order.waitingCount > 0
