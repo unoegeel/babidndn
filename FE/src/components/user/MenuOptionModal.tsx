73c2383a565ed95edcb75dce45c63e47bd1568ab
unoegeel <leegeonu02@gmail.com>
2026-08-20 10:39:18 +0900

    feat: 옵션 시트 가로 스크롤 힌트 추가

--- FE/src/components/user/MenuOptionModal.tsx
+++ FE/src/components/user/MenuOptionModal.tsx
@@ -12,6 +12,7 @@ import {
   trackSavedMenuDeleted,
 } from "../../utils/userEvent/eventHelpers";
 import { SaveMenuPopup } from "./SaveMenuPopup";
+import { HorizontalScrollHintRow } from "./HorizontalScrollHintRow";
 import { USER_PRIMARY_BUTTON_COLOR, userPrimaryButtonClassName } from "./userPrimaryButton";
 
 interface MenuOptionModalProps {
@@ -444,8 +445,9 @@ export const MenuOptionModal: React.FC<MenuOptionModalProps> = ({
           {toppingAddOptions.length > 0 && (
             <div>
               <h3 className="mb-2 text-xs font-bold uppercase tracking-wider text-gray-500">토핑 추가</h3>
-              {/* overflow-x-auto는 세로도 clip → 배지(-top/-right 1.5 = 6px)보다 큰 padding으로 잘림 방지 */}
-              <div className="-mx-1.5 overflow-x-auto px-1.5 pb-1.5 pt-3 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+              <HorizontalScrollHintRow
+                measureKey={`${menuDetail.id}-add-${toppingAddOptions.length}`}
+              >
                 <div className="flex w-max gap-2">
                   {toppingAddOptions.map((opt) => {
                     const qty = selectedOtherOptions[opt.id] || 0;
@@ -523,7 +525,7 @@ export const MenuOptionModal: React.FC<MenuOptionModalProps> = ({
                     );
                   })}
                 </div>
-              </div>
+              </HorizontalScrollHintRow>
             </div>
           )}
 
@@ -562,7 +564,9 @@ export const MenuOptionModal: React.FC<MenuOptionModalProps> = ({
                     })}
                 </div>
               ) : (
-                <div className="-mx-1.5 overflow-x-auto px-1.5 pb-1.5 pt-3 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+                <HorizontalScrollHintRow
+                  measureKey={`${menuDetail.id}-remove-${toppingRemoveOptions.length}`}
+                >
                   <div className="flex w-max gap-2">
                     {[...toppingRemoveOptions]
                       .sort((a, b) => (a.displayOrder ?? 0) - (b.displayOrder ?? 0))
@@ -592,7 +596,7 @@ export const MenuOptionModal: React.FC<MenuOptionModalProps> = ({
                         );
                       })}
                   </div>
-                </div>
+                </HorizontalScrollHintRow>
               )}
             </div>
           )}
